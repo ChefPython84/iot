581bdd82376f8c3a24e46b03ba6bc5d8c26be55a
ChefPython84 <erwan.couturier@etudiant.univ-reims.fr>
2023-10-20 11:34:11 +0200

    feat(main.js): mise en place de la connexion via le cloud

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,31 @@
 // script pour controler la prise plug s
 
 
-const IpAdress = '192.168.1.100' ; // adresse ip de la prise plug s
+const IpAdress = 'shelly-86-eu.shelly.cloud' ; // adresse ip de la prise plug s
 
 // recuperer les infos de la prise plug s
 function getPlugS() {
-    fetch('http://' + IpAdress + '/status')
+    fetch('https://shelly-86-eu.shelly.cloud/device/status?id=80646F827174&auth_key=MWRmYzM2dWlkE62C6C4C76F817CE0A3D2902F5B5D4C115E49B28CF8539114D9246505DE5D368D560D06020A92480')
         .then((response) => {
             return response.json();
         })
         .then((data) => {
             console.log(data);
-            document.getElementById('connected').textContent = data.wifi_sta.connected;
-            document.getElementById('ssid').textContent = data.wifi_sta.ssid;
-            document.getElementById('ip').textContent = data.wifi_sta.ip;
-            document.getElementById('rssi').textContent = data.wifi_sta.rssi;
-            document.getElementById('temperature').textContent = data.temperature;
-            document.getElementById('serial').textContent = data.serial;
-            document.getElementById('mac').textContent = data.mac;
+            document.getElementById('connected').textContent = data.data.device_status.wifi_sta.connected;
+            document.getElementById('ssid').textContent = data.data.device_status.wifi_sta.ssid;
+            document.getElementById('ip').textContent = data.data.device_status.wifi_sta.ip;
+            document.getElementById('rssi').textContent = data.data.device_status.wifi_sta.rssi;
+            document.getElementById('temperature').textContent = data.data.device_status.temperature;
+            document.getElementById('serial').textContent = data.data.device_status.serial;
+            document.getElementById('mac').textContent = data.data.device_status.mac;
         })
         .catch((err) => {
             console.log(err);
         })
 }
 
-function togglePlugS() {
 
-}
+
 
 
 
