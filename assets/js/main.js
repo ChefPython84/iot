d6233377493c62983d3b882b3afaee94157c5918
ChefPython84 <erwan.couturier@etudiant.univ-reims.fr>
2023-10-20 12:17:12 +0200

    feat(main.js): update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
 // script pour controler la prise plug s
 
 
-const IpAdress = 'shelly-86-eu.shelly.cloud' ; // adresse ip de la prise plug s
+const IpAdressData = 'https://shelly-86-eu.shelly.cloud/device/status?id=80646F827174&auth_key=MWRmYzM2dWlkE62C6C4C76F817CE0A3D2902F5B5D4C115E49B28CF8539114D9246505DE5D368D560D06020A92480' ; // adresse ip de la prise plug s
 
 // recuperer les infos de la prise plug s
 function getPlugS() {
-    fetch('https://shelly-86-eu.shelly.cloud/device/status?id=80646F827174&auth_key=MWRmYzM2dWlkE62C6C4C76F817CE0A3D2902F5B5D4C115E49B28CF8539114D9246505DE5D368D560D06020A92480')
+    fetch(IpAdressData)
         .then((response) => {
             return response.json();
         })
@@ -24,8 +24,12 @@ function getPlugS() {
         })
 }
 
+const ipAdressToggle: 'https://shelly-86-eu.shelly.cloud/device/relay/bulk_control?id=80646F827174&auth_key=MWRmYzM2dWlkE62C6C4C76F817CE0A3D2902F5B5D4C115E49B28CF8539114D9246505DE5D368D560D06020A92480'
 
-
+function togglePower() {
+    fetch (ipAdressToggle)
+        .then((response) => {
+}
 
 
 
