06c6afe755473558e7a88fbe6d6f817656121dc8
ChefPython84 <erwan.couturier@etudiant.univ-reims.fr>
2023-11-20 12:37:20 +0100

    feat(index.html): ajout de turn off et turn on

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
-// script pour controler la prise plug s
 
 
 const IpAdressData = 'https://shelly-77-eu.shelly.cloud/device/status?id=4022d88e30e8&auth_key=MWNiMjY5dWlk404459961993DCA83AE44BC6E3A6F58906952E7BECA0A5B69DC375C964915ACBC0EA536A0639CB73' ; // adresse ip de la prise plug s
+const IpAdressRelay = 'https://shelly-77-eu.shelly.cloud/device/relay/control?id=4022d88e30e8&auth_key=MWNiMjY5dWlk404459961993DCA83AE44BC6E3A6F58906952E7BECA0A5B69DC375C964915ACBC0EA536A0639CB73'
 
 // recuperer les infos de la prise plug s
 function getPlugS() {
@@ -26,12 +26,51 @@ function getPlugS() {
         })
 }
 
+// allumer et éteinre la prise plug s
 
+function turnOn() {
+    fetch(IpAdressRelay,{
+        method: 'POST',
+        body: JSON.stringify({
+            'Turn': 'on'
+        })
+    })
+        .then((response) => {
+        return response.json();
+    })
+        .then((data) => {
+        console.log(data);
+        document.getElementById('turn').textContent = data.data.relays[0].ison;
+        }).catch((err) => {
+        console.log(err);
+        })
+}
 
+function turnOff() {
+    fetch(IpAdressRelay,{
+        method: 'POST',
+        body: JSON.stringify({
+            'Turn': 'off'
+        })
+    })
+        .then((response) => {
+        return response.json();
+    })
+        .then((data) => {
+        console.log(data);
+        document.getElementById('turn').textContent = data.data.relays[0].ison;
+        }).catch((err) => {
+        console.log(err);
+        })
+}
 
 
 
 document.addEventListener('DOMContentLoaded', function () {
     getPlugS();
-    setInterval(getPlugS, 1000);
+    document.getElementById('turnOn').addEventListener('click', turnOn);
+    document.getElementById('turnOff').addEventListener('click', turnOff);
+    setInterval(getPlugS,
+        turnOff(),turnOn(),
+        1000);
 });
